c4373605df5e07379b4de720359c9e9ada88ee27
shigeta0506 <113343087+shigeta0506@users.noreply.github.com>
2025-08-21 16:40:46 +0900

    アニメーションを追加

--- My project/Assets/Script/Character/Player.cs
+++ My project/Assets/Script/Character/Player.cs
@@ -8,14 +8,17 @@ public class Player : MonoBehaviour
     private float jumpP = 300f;
 
     Rigidbody2D rbody;
+    Animator animator;
 
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        // ジャンプ入力
         if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rbody.velocity.y) < 0.01f)
         {
             rbody.AddForce(transform.up * jumpP);
@@ -26,15 +29,39 @@ public class Player : MonoBehaviour
     {
         float move = 0f;
 
+        // 左右移動判定と向き
         if (Input.GetKey(KeyCode.D))
         {
             move = 1f;
+            transform.rotation = Quaternion.Euler(0, 0, 0); // 右向き
         }
         else if (Input.GetKey(KeyCode.A))
         {
             move = -1f;
+            transform.rotation = Quaternion.Euler(0, 180, 0); // 左向き
         }
 
+        // 移動処理
         rbody.velocity = new Vector2(move * speed, rbody.velocity.y);
+
+        // アニメーション判定
+        if (Mathf.Abs(rbody.velocity.y) > 0.01f)
+        {
+            // 空中にいるときはジャンプ
+            animator.SetBool("isJumping", true);
+            animator.SetBool("isRunning", false);
+        }
+        else if (Mathf.Abs(move) > 0.01f)
+        {
+            // 地面で移動中
+            animator.SetBool("isJumping", false);
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            // 地面で停止中
+            animator.SetBool("isJumping", false);
+            animator.SetBool("isRunning", false);
+        }
     }
 }
